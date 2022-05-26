14f10e023f9b8758f70955b04243333ce4e09840
Colton127 <coltongrubbs@gmail.com>
2022-05-26 16:35:19 -0400

    Update SwiftTorchControlPlugin.swift

--- ios/Classes/SwiftTorchControlPlugin.swift
+++ ios/Classes/SwiftTorchControlPlugin.swift
@@ -11,12 +11,18 @@ public class SwiftTorchControlPlugin: NSObject, FlutterPlugin {
 
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         if (call.method == "turn") {
-            let state = call.arguments("state") ?: false
-            result.success( turn(state) )
-        } else if (call.method == "hasTorch")
-            result( hasTorch() )
-        else
+            let state = ((call.arguments as! [String: Any])["state"]) as! Bool;
+
+            result(turn(state: state) )
+        } else if (call.method == "ready"){
+            result(hasTorch() )
+              } else if (call.method == "deviceLock"){
+              let lock = ((call.arguments as! [String: Any])["lock"]) as! Bool;
+
+            result(lockDeviceConfiguration(lock: lock) )
+        }else{
             result(FlutterMethodNotImplemented)
+    }
     }
 
     func hasTorch() -> Bool {
@@ -24,11 +30,24 @@ public class SwiftTorchControlPlugin: NSObject, FlutterPlugin {
         return device.hasFlash && device.hasTorch
     }
 
+    func lockDeviceConfiguration(lock: Bool) -> Bool{
+        guard let device = AVCaptureDevice.default(for: .video) else { return false }
+      
+      do {
+ if (lock == true){
+      try  device.lockForConfiguration()
+        }else{
+           try device.unlockForConfiguration()
+        }
+        return true
+         }catch{
+return false
+      }
+    }
+
     func turn(state: Bool) -> Bool {
-        guard hasTorch() else { return false }
-        try! device.lockForConfiguration()
+        guard let device = AVCaptureDevice.default(for: .video) else { return false }
         device.torchMode = state ? .on : .off
-        device.unlockForConfiguration()
         return state
     }
 }
